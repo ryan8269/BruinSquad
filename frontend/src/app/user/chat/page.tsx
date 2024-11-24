2c8ad2d731988786dfeb6b72bbfe7981d26b9f3e
ryan8269 <rchang8269@gmail.com>
2024-11-24 13:17:01 -0800

    Added more information to display (username,time). Display is nicer

--- frontend/src/app/user/chat/page.tsx
+++ frontend/src/app/user/chat/page.tsx
@@ -4,24 +4,33 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { constants } from "fs";
 import { HeartIcon, UsersIcon, UserCircleIcon } from "lucide-react";
 import Link from "next/link";
+import { useSession} from '@clerk/nextjs'
 
 import { io, Socket } from "socket.io-client";
 import { useEffect, useState } from "react";
+import { useUser } from '@clerk/clerk-react'
+import { time } from "console";
 
 const socket: Socket = io("http://localhost:3001"); //FIXME
 
-export default function Home() {
+
+export default function Home() {        
+
     const [message,setMessage] = useState<String>("")
     const [messageReceived, setMessageReceived] = useState<Array<string>>([])
+    const { isSignedIn, user, isLoaded } = useUser()
 
+    
     const sendMessage= () =>{
-        socket.emit("send_message", {message: message});
+        const currentTime = Date.now();
+        socket.emit("send_message", {user: user?.fullName, id: user?.id, message: message, time: currentTime});
+        console.log(user?.fullName);
     }
 
     useEffect(() => {
         socket.on("receive_message", (data) =>
         {
-            setMessageReceived((prev) => {return [...prev,data.message]})
+            setMessageReceived((prev) => {return [...prev,data.user+":"+data.message+" @ "+data.time.toString()]});
         }
         )
     }, [socket])
@@ -32,10 +41,17 @@ export default function Home() {
             <input placeholder ="message..." onChange={(e) => {setMessage(e.currentTarget.value)}}
             />
             <button onClick={sendMessage}> Send Message</button>
+            <h2>
+                Username: {user?.fullName}
+            </h2>
             <h1>
                 Message:
             </h1>
-            {messageReceived}
+            <ul>
+                {messageReceived.map((msg, index) => (
+                    <li key={index}>{msg}</li>
+                ))}
+            </ul>
         </div>
         </>
     );
