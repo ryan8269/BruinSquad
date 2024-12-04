d020852765395fdc7977139c50a9844f7b96ad2d
ryan8269 <rchang8269@gmail.com>
2024-12-03 17:46:31 -0800

    Code cleanup + animations for clicking chat button

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -64,116 +64,34 @@ function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex w-full">
       {/* Sidebar Navigation */}
-      <div className="w-64 bg-blue-500 text-white p-6 space-y-8 h-full">
+      <div className="w-64 bg-blue-500 text-white space-y-8 h-full">
         {/* Button that will direct us to the main page */}
         <Link href="/">
-          <div className="flex items-center space-x-2">
-            <h1 className="text-2xl font-bold">BruinMatch</h1>
+          <div className="p-6">
+            <div className="flex items-center space-x-2">
+              <h1 className="text-2xl font-bold">BruinMatch</h1>
+            </div>
+            <p className="text-sm text-blue-200 mt-2">Connect with Bruins</p>
           </div>
-          <p className="text-sm text-blue-200 mt-2">Connect with Bruins</p>
         </Link>
 
-        <nav className="space-y-4">
+        <nav className="space-y-4 pl-6">
           {/* Once I click on this, direct me to /profile using Link */}
-          {basketball && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("basketball")}
-            >
-              <Image src={BASKETBALL} alt="Basketball" width={50} height={50} />
-              <span > {sportsToChatName["basketball"]} </span>
-            </Link>
-          )}
+          { basketball ? ChatNavBar({ activity:"basketball", imageSource:BASKETBALL, activeRoom, setActiveRoom}) : null}
 
-          {running && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("running")}
-            >
-              <Image src={RUNNING} alt="Running" width={50} height={50} />
-              <span > {sportsToChatName["running"]} </span>
-            </Link>
-          )}
+          { running ? ChatNavBar({ activity:"running", imageSource:RUNNING, activeRoom, setActiveRoom}) : null}
 
-          {tennis && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("tennis")}
-            >
-              <Image src={TENNIS} alt="Tennis" width={50} height={50} />
-              <span> {sportsToChatName["tennis"]} </span>
-            </Link>
-          )}
+          {tennis ? ChatNavBar({ activity:"tennis", imageSource:TENNIS, activeRoom, setActiveRoom}) : null}
 
-          {football && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("football")}
-            >
-              <Image src={FOOTBALL} alt="Football" width={50} height={50} />
-              <span> {sportsToChatName["football"]} </span>
-            </Link>
-          )}
+          {football ? ChatNavBar({ activity:"football", imageSource:FOOTBALL, activeRoom, setActiveRoom}) : null}
 
-          {volleyball && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("volleyball")}
-            >
-              <Image src={VOLLEYBALL} alt="Volleyball" width={50} height={50} />
-              <span> {sportsToChatName["volleyball"]} </span>
-            </Link>
-          )}
+          {volleyball ? ChatNavBar({ activity:"volleyball", imageSource:VOLLEYBALL, activeRoom, setActiveRoom}) : null}
 
-          {badminton && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("badminton")}
-            >
-              <Image src={BADMINTON} alt="Badminton" width={50} height={50} />
-              <span> {sportsToChatName["badminton"]} </span>
-            </Link>
-          )}
+          {badminton ? ChatNavBar({ activity:"badminton", imageSource:BADMINTON, activeRoom, setActiveRoom}) : null}
 
-          {swimming && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("swimming")}
-            >
-              <Image src={SWIMMING} alt="Swimming" width={50} height={50} />
-              <span> {sportsToChatName["swimming"]} </span>
-            </Link> 
-          )}
+          {yoga ? ChatNavBar({ activity:"yoga", imageSource:YOGA, activeRoom, setActiveRoom}) : null}
 
-          {yoga && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("yoga")}
-            >
-              <Image src={YOGA} alt="Yoga" width={50} height={50} />
-              <span> {sportsToChatName["yoga"]} </span>
-            </Link>
-          )}
-
-          {gym && (
-            <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
-              href={"/user/chat"}
-              onClick={() => setActiveRoom("gym")}
-            >
-              <Image src={GYM} alt="Gym" width={50} height={50} />
-              <span> {sportsToChatName["gym"]} </span>
-            </Link>
-          )}
-
+          {gym ? ChatNavBar({ activity:"gym", imageSource:GYM, activeRoom, setActiveRoom}) : null}
         </nav>
       </div>
 
@@ -181,19 +99,22 @@ function MainLayout({ children }: { children: React.ReactNode }) {
       <div className="flex-1 flex flex-col w-full"> {/* Add w-full here */}
         {/* Authentication Controls */}
         <div className="p-4 bg-gray-100 flex items-center">
-            <button
-                className="top-navbar"
-              >
             <Link
-              className="w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600"
+              className={`text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600 text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600`}
               href={"/explore"}
             >
-            <span> Explore </span>
+              <span> Explore </span>
             </Link>
-          </button>
-          <button className = {`${styles.top_navbar}`}>
+          <button className = {`text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600`}>
             EVENTS
           </button>
+                
+            <Link
+              className={`text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600 text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600`}
+              href={"/user/chat"}
+            >
+              <span> Chat </span>
+            </Link>
 
           <SignedOut>
             <SignInButton />
@@ -202,7 +123,7 @@ function MainLayout({ children }: { children: React.ReactNode }) {
             <DropdownMenu.Root>
               <DropdownMenu.Trigger asChild>
               <button
-                className={`${styles.top_navbar} ml-auto`}
+                className={`text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600 ml-auto`}
               >
                 <img
                 src={user?.imageUrl} 
@@ -237,6 +158,29 @@ function MainLayout({ children }: { children: React.ReactNode }) {
       </div>
     </div>
   );
+} 
+
+
+interface ChatNavBarProps {
+  activity: string; 
+  imageSource: any;
+  activeRoom: string | null;
+  setActiveRoom: (room: string) => void;
+}
+
+function ChatNavBar({ activity, imageSource, activeRoom, setActiveRoom}: ChatNavBarProps){
+  return(
+  <Link
+    className= {activeRoom == activity
+      ? `@apply will-change-[opacity,transform] animate-slideLeftAndFade w-full text-black text-left flex items-center space-x-3 p-2 rounded-l bg-white`
+      : `w-full text-left flex items-center space-x-3 p-2 rounded hover:bg-blue-600`
+    }    
+    href={"/user/chat"}
+    onClick={() => setActiveRoom(`${activity}`)}
+    >
+    <Image src={imageSource} alt={`${activity}`} width={50} height={50} />
+    <span> {sportsToChatName[`${activity}`]} </span>
+    </Link>);
 }
 
 interface sportToChat {
